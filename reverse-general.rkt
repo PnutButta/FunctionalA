db17e4068c70a7c2e7b43bb24bc4eb7268fcbc33
Angel Mitchell <angel.mitchell@bison.howard.edu>
2017-01-27 11:49:11 -0500

    syntax reverse general

--- reverse-general.rkt
+++ reverse-general.rkt
@@ -3,11 +3,9 @@
     ((NULL? L) L)
     ((LIST? L)
      (append (reverse-general (CDR L))
-             (LIST (reverse-general (CAR L)))
-     )
+             (LIST (reverse-general (CAR L)))))
+    (ELSE L)
     )
-     (ELSE L)
-   )
   )
 (DEFINE (sum-up-numbers-simple L)
         (COND
@@ -33,12 +31,10 @@
 (DEFINE (min-above-min L1 L2)
         (COND
          ((NULL? L1) #F)
-         ((NOT (NUMBER? (CAR L1))) (min-above-min (CDR L1) L2))
          ((NULL? L2)
           (COND
            ((NULL? (CDR L1)) (CAR L1))
-           ((NOT (NUMBER? (CAR L2))) (min-above-min L1 (CDR L2)))
-           ((< (CAR L1) (min-above-min (CDR L1) L2)) (CAR L1))
+           ((<(CAR L1) (min-above-min (CDR L1) L2)) (CAR L1))
            (ELSE
             (min-above-min (CDR L1) L2))
            )
@@ -47,11 +43,17 @@
           (min-above-min 
            (CONS
             (COND
-              ((> (CAR L1) (min-above-min L2 (LIST))) (CAR L1))
+              ((>(CAR L1) (min-above-min L2 (LIST))) (CAR L1))
               (ELSE
-               (min-above-min (CDR L1) L2))
-            ) (LIST)
-           ) (LIST))
+               (COND
+                ((NULL? (CDR L1))
+                 (min-above-min (LIST) L2))
+                (ELSE
+                 (min-above-min (CDR L1) L2))
+                )
+               )
+            ) (LIST))
+           (LIST))
           )
          )
         )
